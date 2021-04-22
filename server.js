1a5616bb3843553c5b1ecc90652a219bc73fdc58
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2021-04-22 12:16:07 -0400

    Set API_KEY for app bridge from .env file during next build

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ require('isomorphic-fetch');
 const dotenv = require('dotenv');
 const Koa = require('koa');
 const next = require('next');
-const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const { verifyRequest } = require('@shopify/koa-shopify-auth');
-const { default: Shopify, ApiVersion } = require('@shopify/shopify-api');
+const {default: createShopifyAuth} = require('@shopify/koa-shopify-auth');
+const {verifyRequest} = require('@shopify/koa-shopify-auth');
+const {default: Shopify, ApiVersion} = require('@shopify/shopify-api');
 const Router = require('koa-router');
 
 dotenv.config();
@@ -21,7 +21,7 @@ Shopify.Context.initialize({
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const app = next({dev: dev});
 const handle = app.getRequestHandler();
 
 const ACTIVE_SHOPIFY_SHOPS = {};
@@ -34,7 +34,7 @@ app.prepare().then(() => {
   server.use(
     createShopifyAuth({
       afterAuth(ctx) {
-        const { shop, scope } = ctx.state.shopify;
+        const {shop, scope} = ctx.state.shopify;
         ACTIVE_SHOPIFY_SHOPS[shop] = scope;
 
         ctx.redirect(`/?shop=${shop}`);
